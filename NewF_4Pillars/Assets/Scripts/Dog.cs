87110207d10bfc1c21a4bfda2ba18f2a72bbd7b7
dan1989b <109494010+dan1989b@users.noreply.github.com>
2022-09-12 19:34:35 -0500

    Updated Scene
    Changed visuals slightly and implemented more functionality.

---
+++ NewF_4Pillars/Assets/Scripts/Dog.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : Animal //Example of inheritance
+{
+    private string m_displayName;
+
+    public string DisplayName //Example of encapsulation
+    {
+        get { return m_displayName; }
+
+        set
+        {
+            if (value.Length > 10)
+            {
+                Debug.LogError("Name cannot be longer than 10 chars");
+            }
+            else 
+            { m_displayName = value; }
+
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        DisplayName = "Dog";
+        Debug.Log(DisplayName);
+        MakeSound();
+        Jump();
+    }
+
+
+    public override void MakeSound() //Example of polymorphism
+    {
+        Debug.Log("Dog barked!!!");
+    }
+
+    public override void Jump()
+    {
+        Debug.Log("Dog jumped 2 feet!");
+    }
+}
